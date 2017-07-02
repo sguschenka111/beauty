ae4af8c4f626f69a598c73478ea0c524a87b1048
Anastasia Dementeva <nastya.dementeva.2016@mail.ru>
2017-07-02 18:33:15 +0300

    More edits

--- app/js/msk_aksia.js
+++ app/js/msk_aksia.js
@@ -21,7 +21,29 @@ var slider2 = new Swiper('.mobile-card-shares-container', {
     prevButton: '.prev2'
 });
 
+ $('.mobile-salon-nav__item').on('click',function(e){
+        e.preventDefault();
+        
+        let item = $(this),
+            contentItem = $('.mobile-salon-content'),
+            itemPosition = item.data('class');
+            
+           contentItem.filter('.mobile-salon_' + itemPosition).add(item)
+           .addClass('mobile-salon-nav__item_active')
+           .siblings().removeClass('mobile-salon-nav__item_active');
+        
+    });
+    
+    $('.mobile-further-salon').on('click',function(e){
+        e.preventDefault();
+        $('.mobile-description-salon-content_p').css('display','block');   
+        $(this).css('display','none');
+    });
+
 }());
+
+ 
+   
  
    
     
